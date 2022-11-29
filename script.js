5a2e2fed65980651eb2a0cde7623650cc3dbe687
SkTs61 <110562168+SkTs61@users.noreply.github.com>
2022-11-30 00:46:26 +0300

    final ground

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+window.addEventListener("load", function () {
+   const dataFromLocalStorage = JSON.parse(localStorage.getItem("islogin"));
+   console.log(dataFromLocalStorage);
+   if (dataFromLocalStorage === null) {
+    localStorage.setItem("isLogin", JSON.stringify(false));
+   }
+   if (dataFromLocalStorage === null || dataFromLocalStorage === false) {
+    window.location.replace("http://127.0.0.1:5500/login.html");
+   }
+}
+
+);
+
 const pdfbtn=document.getElementById("pdfbtn")
 const languageSelect=document.getElementById("language");
+const logout=document.getElementById("logout");
+
+logout.addEventListener("click", function(){
+    localStorage.setItem("isLogin", JSON.stringify(false));
+    window.location.reload();
+})
 
 
 const dictionary = [
